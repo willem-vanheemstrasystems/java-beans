4f44488fc1a180a50a2c222b19f3c159a708d7c5
wvanheemstra <willem@vanheemstrasystems.com>
2017-05-23 14:02:47 +0200

    Update

---
+++ spring-tutorial-14/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -0,0 +1,16 @@
+package com.caveofprogramming.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
+		
+		FruitBasket basket = (FruitBasket)context.getBean("basket");
+		
+		System.out.println(basket);
+		
+		((ClassPathXmlApplicationContext)context).close();
+	}
+}
